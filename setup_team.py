931c38f195bc0879a13defa07be6c0f5ec87dc67
Marie-Ève Ferland-Bernier <245264793+mferlandbernier-creator@users.noreply.github.com>
2025-12-19 11:39:29 -0500

    Mise à jour de la nommination des branches

---
+++ setup_team.py
@@ -0,0 +1,12 @@
+# setup_team.py - À exécuter par le chef de projet
+def initialiser_projet():
+	print("=== INITIALISATION DU PROJET ===")
+	print("1. Créer dépôt GitHub")
+	print("2. Cloner sur toutes les machines")
+	print("3. Créer branches :")
+	print("   - git checkout -b feature-jeu Marie-Ève Ferland-Bernier")
+	print("   - git checkout -b feature-affichage Jeremy Noivo")
+	print("   - git checkout -b feature-choisir-mot Jean-Simon Cyr")
+	print("   - git checkout -b eature-main Eva Bessette")
+	
+	print("4. Commencer à coder !")
